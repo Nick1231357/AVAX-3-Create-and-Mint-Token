f8ee3a1fc3f848015407e7805de9267122e47fe3
Nick1231357 <114212806+Nick1231357@users.noreply.github.com>
2023-11-20 21:24:42 +0800

    Add files via upload

---
+++ Token.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+
+
+contract Token is ERC20 {
+    address public Owner;
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        Owner = msg.sender;
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mintTokens(address account, uint256 amount) public {
+        require(msg.sender == Owner, "Access Denied: Only the Owner can access this.");
+        _mint(account, amount);
+    }
+
+    function burnTokens(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
+}
